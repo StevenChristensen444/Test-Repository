cfab9e8829954871868e07bbf6525b4d228577b1
Steven Christensen <stevenchristensen4@mail.weber.edu>
2025-09-02 23:42:53 +0000

    committing to repo

---
+++ Hello_World.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/bash
+# print hello world to terminal
+echo "Hello World!"
+# Print message
+echo "Scripting is fun and frustrating"
+# print current dir
+echo "The current working directory is: $(pwd)"
+# create Temp folder if Temp doesn't exist
+if [ ! -d Temp ];
+then
+    echo "Temp directory does not exist. Making Temp directory"
+    mkdir Temp
+# Print message directory already exists
+else
+    echo "Temp already exists"
+fi 
+# change to Temp directory
+cd Temp
+echo "This is the current directory: $(pwd)"
+# create new file
+touch NewFile.txt
+ls
